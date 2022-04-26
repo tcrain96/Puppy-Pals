a25bf340e57146c8e76579888c1dee9c396bad97
tcrain96 <tristan.crain@outlook.com>
2022-04-25 20:02:52 -0400

    added fix to server file that now allows it to run on other machines with a relative path

--- server/server.js
+++ server/server.js
@@ -42,7 +42,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
 });
 
 db.once("open", () => {
