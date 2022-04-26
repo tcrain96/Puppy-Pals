3fd4a5febce3b19db597280db92a59b673029e26
tcrain96 <tristan.crain@outlook.com>
2022-04-25 20:17:18 -0400

    removed app.get all to fix bug

--- server/server.js
+++ server/server.js
@@ -41,11 +41,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("*", (req, res) => {
-  const index = path.join(__dirname, "build", "index.html");
-  res.sendFile(index);
-});
-
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
