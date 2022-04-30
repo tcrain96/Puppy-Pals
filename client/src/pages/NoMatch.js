14759685efa39263b368f6bdd882dbc36a6e424b
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-04-30 10:21:31 -0400

    added cute dog emoji on 404 page and also fixed text styling

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -4,7 +4,7 @@ import "./css/NoMatch.css";
 const Login = () => {
   return (
     <article>
-      <div className="header">Page Not Found!</div>
+      <div className="header">Page Not Found! 🐕</div>
       <div className="text">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or look at our documentation.</div>
     </article>
   );
