aa1b9256aa881ea4fae1de2d6e7e62c3306709e8
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-04-30 10:18:35 -0400

    changed 404 page header styling

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -4,8 +4,8 @@ import "./css/NoMatch.css";
 const Login = () => {
   return (
     <article>
-      <div className="col-12 mb-3" id="404-header">Page Not Found!</div>
-      <div className="col-12 mb-3" id="404-text">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or look at our documentation.</div>
+      <div className="header">Page Not Found!</div>
+      <div className="text">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or look at our documentation.</div>
     </article>
   );
 };
