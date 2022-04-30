e242a8593ab3511f19b7f30d1e2226ab4b3509de
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-04-30 10:11:31 -0400

    made html changes to 404 page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -4,7 +4,8 @@ import "./css/NoMatch.css";
 const Login = () => {
   return (
     <article>
-      <div className="col-12 mb-3">I am the NoMatch page!</div>
+      <div className="col-12 mb-3" id="404-header">Page Not Found!</div>
+      <div className="col-12 mb-3" id="404-text">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or look at our documentation.</div>
     </article>
   );
 };
