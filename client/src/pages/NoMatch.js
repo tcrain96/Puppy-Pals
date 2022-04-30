6228033cabe3d29925f180eca8820d36964f8d53
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-04-30 09:55:34 -0400

    created NoMatch styling sheet

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./css/NoMatch.css";
 
 const Login = () => {
   return (
-    <div className="flex-row justify-space-between">
+    <article>
       <div className="col-12 mb-3">I am the NoMatch page!</div>
-    </div>
+    </article>
   );
 };
 
