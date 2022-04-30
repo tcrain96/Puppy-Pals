f1238e8f3bb895dfc86d80e1ef3714d6eec084ab
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-30 15:49:48 -0400

    Worked on Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -22,64 +22,30 @@ const Profile = () => {
 
   const user = data?.me || data?.user || {};
 
-// redirect to personal profile page if username is yours
-if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-  return <Redirect to="/profile" />;
-}
-
-if (loading) {
-  return <div>Loading...</div>;
-}
-
-if (!user?.username) {
-  return (
-    <h4>
-      You need to be logged in to see this. Use the navigation links above to
-      sign up or log in!
-    </h4>
-  );
-}
-
-
-const handleClick = async () => {
-  try {
-    await addEvent({
-      variables: { id: user._id },
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-
 
   return (
-    <div className="flex-row justify-space-between">
-      <div className="col-12 mb-3">I am the profile page!</div>
+    <div>
+      <div>I am the profile page!</div>
 
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+      <div>
+        <h2>
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
 
+
+
         {userParam && (
-          <button className="btn ml-auto" onClick={handleClick}>
+          <button >
             Add Event
           </button>
         )}
       </div>
 
 
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">
-          <EventList
-            events={user.events}
-            title={`${user.username}'s Events...`}
-          />
-        </div>
+     
 
         
-      </div>
+
      
     </div>
   );
