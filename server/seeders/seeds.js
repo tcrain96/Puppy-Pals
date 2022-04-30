69f0b75fbed1c2612b5b4462b6b1377543a6fa3a
tcrain96 <tristan.crain@outlook.com>
2022-04-29 20:53:50 -0400

    fixed seeder file to create users, dogs and events

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,11 +1,12 @@
-const faker = require('faker');
+const faker = require("faker");
 
-const db = require('../config/connection');
-const { Thought, User } = require('../models');
+const db = require("../config/connection");
+const { User, Dog, Event } = require("../models");
 
-db.once('open', async () => {
-  await Thought.deleteMany({});
+db.once("open", async () => {
+  await Dog.deleteMany({});
   await User.deleteMany({});
+  await Event.deleteMany({});
 
   // create user data
   const userData = [];
@@ -20,56 +21,60 @@ db.once('open', async () => {
 
   const createdUsers = await User.collection.insertMany(userData);
 
-  // create friends
+  // create dogs
+  let createdDogs = [];
   for (let i = 0; i < 100; i += 1) {
-    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { _id: userId } = createdUsers.ops[randomUserIndex];
-
-    let friendId = userId;
-
-    while (friendId === userId) {
-      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-      friendId = createdUsers.ops[randomUserIndex];
-    }
-
-    await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
-  }
-
-  // create thoughts
-  let createdThoughts = [];
-  for (let i = 0; i < 100; i += 1) {
-    const thoughtText = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const dogName = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const dogAge = (Math.random() * 20).toString();
+    const dogGender = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const dogDescription = faker.lorem.words(
+      Math.round(Math.random() * 20) + 1
+    );
+    const dogNeut = true;
 
     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
     const { username, _id: userId } = createdUsers.ops[randomUserIndex];
 
-    const createdThought = await Thought.create({ thoughtText, username });
+    const createdDog = await Dog.create({
+      name: dogName,
+      age: dogAge,
+      gender: dogGender,
+      description: dogDescription,
+      neuteredOrSpade: dogNeut,
+    });
 
     const updatedUser = await User.updateOne(
       { _id: userId },
-      { $push: { thoughts: createdThought._id } }
+      { $push: { dogs: createdDog._id } }
     );
 
-    createdThoughts.push(createdThought);
+    createdDogs.push(createdDog);
   }
 
-  // create reactions
+  // create events
+  let createdEvents = [];
   for (let i = 0; i < 100; i += 1) {
-    const reactionBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const eventDate = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const eventTime = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+    const eventLocation = faker.lorem.words(Math.round(Math.random() * 20) + 1);
 
     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { username } = createdUsers.ops[randomUserIndex];
+    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
 
-    const randomThoughtIndex = Math.floor(Math.random() * createdThoughts.length);
-    const { _id: thoughtId } = createdThoughts[randomThoughtIndex];
+    const createdEvent = await Event.create({
+      date: eventDate,
+      time: eventTime,
+      location: eventLocation,
+    });
 
-    await Thought.updateOne(
-      { _id: thoughtId },
-      { $push: { reactions: { reactionBody, username } } },
-      { runValidators: true }
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { dogs: createdEvent._id } }
     );
+
+    createdEvents.push(createdEvent);
   }
 
-  console.log('all done!');
+  console.log("all done!");
   process.exit(0);
 });
