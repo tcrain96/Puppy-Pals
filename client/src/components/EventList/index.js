d66b79cf0098d8ac0f6d275fb48adf4f095e9367
tcrain96 <tristan.crain@outlook.com>
2022-04-30 18:02:58 -0400

    added styles to nav bar and dashboard

--- client/src/components/EventList/index.js
+++ client/src/components/EventList/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import "./index.css";
+import { useQuery } from "@apollo/client";
+import { QUERY_ME_BASIC } from "../../utils/queries";
 
 const EventList = ({ events }) => {
   if (!events.length) {
@@ -6,7 +9,7 @@ const EventList = ({ events }) => {
   }
 
   return (
-    <section>
+    <section className="event-list-section">
       {events &&
         events.map((event) => (
           <article key="event._id">
