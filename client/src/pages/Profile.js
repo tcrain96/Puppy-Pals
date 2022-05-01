c6157072ce317ebdc74f54b43404ed4b8a6c8faa
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-05-01 00:00:41 -0400

    worked on displaying user info

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,18 +5,34 @@ import { useQuery, useMutation } from "@apollo/client";
 
 import EventList from "../components/EventList";
 
-import { GET_USER, GET_USERS } from "../utils/queries";
+import { GET_USER, QUERY_ME} from "../utils/queries";
+import { GET_DOG, QUERRY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
 import { ADD_EVENT } from "../utils/mutations";
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(userParam ? GET_USER : GET_USERS, {
-    variables: { username: userParam },
+  
+  const { loading, data } = useQuery(userParam ? GET_USER : QUERY_ME, {
+    variables: { username: userParam},
   });
 
-  const user = data?.me || data?.user || {};
+  const user = data?.me || data?.user || data?.dog || {};
+
+
+
+
+
+  // const {id: useParams} = useParams();
+
+
+
+  // const id = data?.me || data?.id || {};
+
+
+
+
 
   return (
     <div>
@@ -24,10 +40,25 @@ const Profile = () => {
 
       <div>
         <h2>Viewing {userParam ? `${user.username}'s` : "your"} profile.</h2>
+        {/* <h2>Dog: {userParam ? `${dog.name}` : "lol" }</h2> */}
 
         {userParam && <button>Add Event</button>}
       </div>
+
+
+
+       <div>
+
+      
+
+      </div>
+
+
     </div>
+
+ 
+
+
   );
 };
 
